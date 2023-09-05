4aea1c86a6d9daf5c5771e10179bc839948c54ce
LucianoR23 <231097lr@gmail.com>
2023-09-05 17:21:35 -0300

    Probando altura en celular

--- src/layout/PortfolioLayout.jsx
+++ src/layout/PortfolioLayout.jsx
@@ -56,7 +56,7 @@ export const PortfolioLayout = ({ children }) => {
             <video playsInline={true} autoPlay={false} muted style={{ position: 'absolute', width: '100%', height: '100%', objectFit: 'cover', zIndex: -1 }}>
                 <source src="../../video/fondo-mundo.mp4" type="video/mp4" />
             </video>
-            <Box className='animate__animated animate__backInUp'  component='main' sx={{ p: 3, height: '90vh',  }} display='flex' flexWrap='wrap' alignContent='space-between'>
+            <Box className='animate__animated animate__backInUp'  component='main' sx={{ p: 3, height: '85vh',  }} display='flex' flexWrap='wrap' alignContent='space-between'>
 
               <AccordionMenu { ...prop } />
                 <Toolbar />
